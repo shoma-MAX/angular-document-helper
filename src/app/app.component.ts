ae6dbd54cc2ce623a59be70518e585f6b8f27449
shoma-MAX <shomakatsura@gmail.com>
2020-09-29 21:49:15 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  public inputarea
+  outputarea
+  public message;
+  public inputarray:string[]
+  
+  
+ ngDoCheck() {
+    console.log('Running change detection ', Date.now());
+   // console.log(this.inputarea);
+  }
+  input(){
+
+    this.inputarea.length
+    const inputarray=this.inputarea.split("。");
+    console.log(inputarray[2])
+    console.log(this.inputarea.length)
+    if(this.inputarea.length>10){
+      this.message="文字数が長すぎます"
+    }
+  }
+
+
 }
