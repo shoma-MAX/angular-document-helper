e7b49cfe5ad268efee51deedfc7d12ecf6f1abb2
shoma-MAX <shomakatsura@gmail.com>
2020-09-30 11:30:51 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,33 @@ export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   public inputarea
   outputarea
-  public message;
-  public inputarray:string[]
+  public message:string[];
+  public inputarray:string[];
+  public upperlength:number;
+
+  
   
   
  ngDoCheck() {
-    console.log('Running change detection ', Date.now());
+    //console.log('Running change detection ', Date.now());
    // console.log(this.inputarea);
   }
   input(){
 
     this.inputarea.length
+
+    //"。"で区切って配列に収容している。
     const inputarray=this.inputarea.split("。");
-    console.log(inputarray[2])
-    console.log(this.inputarea.length)
-    if(this.inputarea.length>10){
-      this.message="文字数が長すぎます"
+    //"。"で区切った3つ目の文字列をコンソールに出力するテストコード
+   // console.log(inputarray [2].length)
+    //console.log(this.inputarea.length)
+    for(var i=0;i<inputarray.length;i++){
+    if(inputarray[i].length>this.upperlength){
+      this.message[i]="1文あたりの文字数が長すぎます"
+    }else{
+      this.message[i]=""
+      //IDEA:messageも配列にすればいい。配列にしたうえで、aggridかngForで表示する。
+    }//あまりいい実装とはいえない、。ngIfなどほかの方法を検討するべき。
     }
   }
 
