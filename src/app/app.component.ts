81fc9895d7f3e4d110030ee98c5fd558458f96de
shoma-MAX <shomakatsura@gmail.com>
2020-10-03 16:59:44 +0900

    指示語に関する注意喚起を実装。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,59 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public inputarea;
+  outputarea;
+  public message: string[];
+  public inputarray: string[];
+  public upperlength: number = 20;
+  public output: string;
+  public tooLongBoolean: boolean;
+  public gobiBoolean: boolean;
+  public gobiList: string[][];
+  public demonstrative: string[] = ["それ", "これ", "あれ", "その","そこ","あそこ","あの"];
+  public demoBoolean: boolean;
 
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  public inputarea
-  outputarea
-  public message:string[];
-  public inputarray:string[];
-  public upperlength:number=20;
-  public output:string;
-  public tooLongBoolean:boolean;
-
-  
-  
-  
- ngDoCheck() {
+  ngDoCheck() {
     //console.log('Running change detection ', Date.now());
-   // console.log(this.inputarea);
+    // console.log(this.inputarea);
   }
-  input(){
-
-   // this.inputarea.length
+  input() {
+    // this.inputarea.length
 
     //"。"で区切って配列に収容している。
-    const inputarray=this.inputarea.split("。");
-    console.log(inputarray)
+    const inputarray = this.inputarea.split("。");
+    //console.log(inputarray)
     //"。"で区切った3つ目の文字列をコンソールに出力するテストコード
-   // console.log(inputarray [2].length)
+    // console.log(inputarray [2].length)
     //console.log(this.inputarea.length)
-    for(var i=0;i<inputarray.length;i++){
-      var j=0;
-      console.log(inputarray[i].length)
-      if(inputarray[i].length>this.upperlength){
-        this.tooLongBoolean=true;
+    for (var i = 0; i < inputarray.length; i++) {
+      var j = 0;
+      // console.log(inputarray[i].length)
+      if (inputarray[i].length > this.upperlength) {
+        this.tooLongBoolean = true;
         break;
-      }else{
-        this.tooLongBoolean=false;
+      } else {
+        this.tooLongBoolean = false;
       }
-      
-      
     }
-     
-     
-   
-   
-  
+
+    for (var i = 0; i < inputarray.length; i++) {
+      // console.log(inputarray[i].length)
+      for (var j = 0; j < this.demonstrative.length; j++) {
+        console.log(inputarray[i].indexOf(this.demonstrative[j]));
+        if (inputarray[i].indexOf(this.demonstrative[j]) != -1) {
+          this.demoBoolean = true;
+          break;
+        } else {
+          this.demoBoolean = false;
+        }
+      }
+    }
   }
 }
-  
-
-
-
